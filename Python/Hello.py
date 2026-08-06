eb5dd76719234151350cb8e5846d08272c5fcde7
Tobias Andersson <t0bb3.andersson@gmail.com>
2026-08-06 16:29:04 +0200

    Första Python-övningarna

---
+++ Python/Hello.py
@@ -0,0 +1,2 @@
+print("Hello, world!")
+print("Nu har vi tagit första steget in i kurserna på BTH, nu är det bara att fortsätta förberedelserna inför kursstart")
---
+++ Python/Variables.py
@@ -0,0 +1,7 @@
+Namn = input("Vad heter du? ")
+Ålder = input("Hur gammal är du? ")
+Jobb = input("Vad jobbar du med? ")
+
+print("Hej! Jag heter", Namn)
+print("Jag är", Ålder, "år gammal")
+print("Idag jobbar jag som", Jobb, "men målet är att inom en snar framtid börja som mjukvaruutveklare")
